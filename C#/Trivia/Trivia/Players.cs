abb360b1fd074af157f2018c7eccfe9983d87608
LEBLOIS VINCENT p1402522 <p1402522@univ-lyon1.fr>
2017-03-23 17:56:46 +0100

    Création de Players

---
+++ C#/Trivia/Trivia/Players.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia
+{
+    public class Players
+    {
+        private List<Player> _players { get; set; }
+
+        public Player Current { get; private set; }
+
+        public bool Add(string playerName)
+        {
+            var player = new Player(playerName);
+
+            if (_players == null)
+            {
+                _players = new List<Player>();
+                Current = player;
+            }
+
+            _players.Add(player);
+
+            Console.WriteLine(playerName + " was added");
+            Console.WriteLine("They are player number " + _players.Count);
+            return true;
+        }
+
+        public void NextPlayer()
+        {
+            var nextPlayer = _players.IndexOf(Current) + 1;
+            if (nextPlayer == _players.Count)
+                nextPlayer = 0;
+            Current = _players[nextPlayer];
+        }
+
+    }
+}
