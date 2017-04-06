a19c44bd363d3b097b800257efe8a33c6ca1b058
LEBLOIS VINCENT p1402522 <p1402522@univ-lyon1.fr>
2017-04-06 17:07:18 +0200

    Creation QuestionStack

--- C#/Trivia/Trivia/Game.cs
+++ C#/Trivia/Trivia/Game.cs
@@ -6,33 +6,92 @@ namespace Trivia
 {
     public class Game
     {
-        private readonly Dictionary<int, string> _categories = new Dictionary<int, string>() {{0, "Pop"}, {1, "Science"}, {2, "Sports"}, {3, "Rock"}};
+        private readonly Dictionary<int, string> _categories = new Dictionary<int, string>() { { 0, "Pop" }, { 1, "Science" }, { 2, "Sports" }, { 3, "Rock" } };
+
+        QuestionStack popQuestions = new QuestionStack("Pop");
+        QuestionStack scienceQuestions = new QuestionStack("Science");
+        QuestionStack sportsQuestions = new QuestionStack("Sports");
+        QuestionStack rockQuestions = new QuestionStack("Rock");
 
         private readonly Players _players;
-
-        LinkedList<string> popQuestions = new LinkedList<string>();
-        LinkedList<string> scienceQuestions = new LinkedList<string>();
-        LinkedList<string> sportsQuestions = new LinkedList<string>();
-        LinkedList<string> rockQuestions = new LinkedList<string>();
-
-        bool isGettingOutOfPenaltyBox;
-
 
         public Game(Players players)
         {
             _players = players;
             for (var i = 0; i < 50; i++)
             {
-                popQuestions.AddLast("Pop Question " + i);
-                scienceQuestions.AddLast(("Science Question " + i));
-                sportsQuestions.AddLast(("Sports Question " + i));
-                rockQuestions.AddLast(CreateRockQuestion(i));
+                popQuestions.Generate(i);
+                scienceQuestions.Generate(i);
+                sportsQuestions.Generate(i);
+                rockQuestions.Generate(i);
             }
         }
-        
-        public string CreateRockQuestion(int index)
+
+        public bool WasCorrectlyAnswered()
         {
-            return "Rock Question " + index;
+            bool winner;
+            if (_players.Current.InPenaltyBox)
+            {
+                if (isGettingOutOfPenaltyBox)
+                {
+                    Console.WriteLine("Answer was correct!!!!");
+                    _players.Current.WinAGoldCoin();
+
+                    winner = _players.Current.IsWinner();
+                    _players.NextPlayer();
+
+                    return winner;
+                }
+
+                _players.NextPlayer();
+                return false;
+            }
+
+            Console.WriteLine("Answer was corrent!!!!");
+            _players.Current.WinAGoldCoin();
+
+            winner = _players.Current.IsWinner();
+            _players.NextPlayer();
+
+            return winner;
+        }
+
+        public bool WrongAnswer()
+        {
+            Console.WriteLine("Question was incorrectly answered");
+            Console.WriteLine(_players.Current.Name + " was sent to the penalty box");
+            _players.Current.GoToPenaltyBox();
+
+            _players.NextPlayer();
+            return false;
+        }
+
+        bool isGettingOutOfPenaltyBox;
+
+        private string CurrentCategory()
+        {
+            return _categories[_players.Current.Place % 4];
+        }
+
+
+        private void AskQuestion()
+        {
+            if (CurrentCategory() == "Pop")
+            {
+                popQuestions.PickAndReadAQuestion();
+            }
+            if (CurrentCategory() == "Science")
+            {
+                scienceQuestions.PickAndReadAQuestion();
+            }
+            if (CurrentCategory() == "Sports")
+            {
+                sportsQuestions.PickAndReadAQuestion();
+            }
+            if (CurrentCategory() == "Rock")
+            {
+                rockQuestions.PickAndReadAQuestion();
+            }
         }
 
         public void Roll(int roll)
@@ -75,73 +134,6 @@ namespace Trivia
 
         }
 
-        private void AskQuestion()
-        {
-            if (CurrentCategory() == "Pop")
-            {
-                Console.WriteLine(popQuestions.First());
-                popQuestions.RemoveFirst();
-            }
-            if (CurrentCategory() == "Science")
-            {
-                Console.WriteLine(scienceQuestions.First());
-                scienceQuestions.RemoveFirst();
-            }
-            if (CurrentCategory() == "Sports")
-            {
-                Console.WriteLine(sportsQuestions.First());
-                sportsQuestions.RemoveFirst();
-            }
-            if (CurrentCategory() == "Rock")
-            {
-                Console.WriteLine(rockQuestions.First());
-                rockQuestions.RemoveFirst();
-            }
-        }
-
-
-        private string CurrentCategory()
-        {
-            return _categories[_players.Current.Place % 4];
-        }
-
-        public bool WasCorrectlyAnswered()
-        {
-            bool winner;
-            if (_players.Current.InPenaltyBox)
-            {
-                if (isGettingOutOfPenaltyBox)
-                {
-                    Console.WriteLine("Answer was correct!!!!");
-                    _players.Current.WinAGoldCoin();
-
-                    winner = _players.Current.IsWinner();
-                    _players.NextPlayer();
-
-                    return winner;
-                }
-
-                _players.NextPlayer();
-                return false;
-            }
-
-            Console.WriteLine("Answer was corrent!!!!");
-            _players.Current.WinAGoldCoin();
-
-            winner = _players.Current.IsWinner();
-            _players.NextPlayer();
-
-            return winner;
-        }
-
-        public bool WrongAnswer()
-        {
-            Console.WriteLine("Question was incorrectly answered");
-            Console.WriteLine(_players.Current.Name + " was sent to the penalty box");
-            _players.Current.GoToPenaltyBox();
-
-            _players.NextPlayer();
-            return false;
-        }
+        
     }
 }
---
+++ C#/Trivia/Trivia/QuestionStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia
+{
+    class QuestionStack
+    {
+        private LinkedList<string> questions = new LinkedList<string>();
+        private string category;
+
+        public QuestionStack(string category)
+        {
+            this.category = category;
+        }
+
+
+        public void Generate(int indexQuestion)
+        {
+            questions.AddLast(category + " Question " + indexQuestion);
+        }
+
+        public void PickAndReadAQuestion()
+        {
+            Console.WriteLine(questions.First());
+            questions.RemoveFirst();
+        }
+    }
+}
